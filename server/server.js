10ac8b81f4e137f783ed694a906ab1268545d4d2
Danya D. Smith <danyadsmith@gmail.com>
2017-03-13 15:32:50 -0400

    Initial Client/Server Setup Using the MEAN Stack

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+
+var app =  express();
+//mongoose.connect('mongodb://localhost/scope-hero');
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+var port = 5459;
+
+app.listen(port);
+console.log('Server now listening on port ' + port);
