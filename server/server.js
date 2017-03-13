a5217a0b0459e19a4438370107d0bfc444973298
Danya D. Smith <danyadsmith@gmail.com>
2017-03-13 19:08:39 -0400

    Updates Basic Server Configuration
    Moves all database related commands to db.js
    Adds require statements for configuration files.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var mongoose = require('mongoose');
+require('./config/db.js');
+require('./config/router.js');
 
 var app =  express();
-mongoose.connect('mongodb://localhost/scope-hero');
 
 app.use(bodyParser.json());
 app.use(express.static('client'));
+
 
 var port = 5459;
 
